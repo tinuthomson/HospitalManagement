87229e464aefb8c48ab7416c4532d09831eb1ce2
anson <anzonvarghese@gmail.com>
2018-01-18 19:52:25 +0530

    feedback added.But not linked

---
+++ app/src/main/java/com/zibbix/hospital/hospitalmanagement/FeedbackActivity.java
@@ -0,0 +1,27 @@
+package com.zibbix.hospital.hospitalmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class FeedbackActivity extends AppCompatActivity {
+    Button btn;
+    RatingBar ratingBar;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        btn=(Button)findViewById(R.id.feedback_button);
+        editText=(EditText)findViewById(R.id.feedbacktxt);
+        ratingBar =(RatingBar)findViewById(R.id.ratingBar);
+                              }
+
+public void BtnClick(View v)
+{    float ratingvalue=ratingBar.getRating();
+    Toast.makeText(this,"your feedback is "+ratingvalue,Toast.LENGTH_SHORT).show();
+}}
