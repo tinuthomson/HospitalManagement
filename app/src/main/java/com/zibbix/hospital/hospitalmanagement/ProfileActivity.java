481932bf2187784de2880df8f12f4c4511827988
scorpio <tinuthomson@gmail.com>
2018-01-23 21:21:16 +0530

    profile activity

--- app/src/main/java/com/zibbix/hospital/hospitalmanagement/ProfileActivity.java
+++ app/src/main/java/com/zibbix/hospital/hospitalmanagement/ProfileActivity.java
@@ -1,11 +1,7 @@
 package com.zibbix.hospital.hospitalmanagement;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.FrameLayout;
 
 public class ProfileActivity extends AppointActivity {
@@ -16,15 +12,8 @@ public class ProfileActivity extends AppointActivity {
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
         getLayoutInflater().inflate(R.layout.activity_profile, contentFrameLayout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Medicive");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
