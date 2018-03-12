374765d2722ed16b6b51ccccdbfe8f8719a50b93
scorpio <tinuthomson@gmail.com>
2018-03-12 23:50:56 +0530

    h

--- app/src/main/java/com/zibbix/hospital/hospitalmanagement/ConsultActivity.java
+++ app/src/main/java/com/zibbix/hospital/hospitalmanagement/ConsultActivity.java
@@ -2,6 +2,7 @@ package com.zibbix.hospital.hospitalmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,19 +26,26 @@ public class ConsultActivity extends AppCompatActivity {
         final FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("dd-MM-yyyy");
         Calendar cal = Calendar.getInstance();
+        final TextView counter = (TextView)findViewById(R.id.textView7);
+        final TextView ucounter = (TextView)findViewById(R.id.textView11);
 
 
 
         DatabaseReference databaseRefUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentFirebaseUser.getUid()).child("Appointment");
         databaseRefUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(final DataSnapshot dataSnapshot) {
 
                 DatabaseReference databaseRefdoc = FirebaseDatabase.getInstance().getReference().child("Doctors").child("2tHlValmqGTLHHFHyQV9GFr7NBo1").child("counter");
                 databaseRefdoc.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dSnapshot) {
                         long c = (long) dSnapshot.getValue();
+                        for (DataSnapshot Snapshot : dataSnapshot.getChildren()) {
+                            counter.setText(String.valueOf(c));
+                            ucounter.setText(String.valueOf(Snapshot.getValue().toString()));
+
+                        }
 
                     }
 
