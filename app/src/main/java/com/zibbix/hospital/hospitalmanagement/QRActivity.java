26f116316a6225ca22f905f8f2744839f63cd71e
scorpio <tinuthomson@gmail.com>
2018-03-19 14:03:54 +0530

    latest working

--- app/src/main/java/com/zibbix/hospital/hospitalmanagement/QRActivity.java
+++ app/src/main/java/com/zibbix/hospital/hospitalmanagement/QRActivity.java
@@ -3,6 +3,9 @@ package com.zibbix.hospital.hospitalmanagement;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -15,17 +18,30 @@ import org.json.JSONObject;
 public class QRActivity extends AppCompatActivity {
 
     private IntentIntegrator qrScan;
+    TextView amount ;
+    Button pay ;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+        amount = (TextView) findViewById(R.id.amount);
+        pay = (Button) findViewById(R.id.pay);
         qrScan = new IntentIntegrator(this);
         qrScan.setCaptureActivity(CaptureActivityPortait.class);
 
         qrScan.initiateScan();
 
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CompleteActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
     //Getting the scan results
@@ -49,10 +65,13 @@ public class QRActivity extends AppCompatActivity {
                     //in this case you can display whatever data is available on the qrcode
                     //to a toast
                     Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                    amount.setText(result.getContents());
                 }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+
 }
